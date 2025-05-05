0fcc514bbbff4c1c75d4ebcbab937125070d182a
Hossein-Hmb <h.hajmirbaba@gmail.com>
2025-05-05 13:45:38 -0400

    Added Class Paddle and Ball | Added speed to ball | speed boost so speed increases | paddles have limit so they dont cross the display

--- pong.py
+++ pong.py
@@ -1,5 +1,5 @@
 import pygame
-import random
+
 
 # Initialize Pygame
 pygame.init()
@@ -7,17 +7,48 @@ screen = pygame.display.set_mode((1280, 720))
 running = True
 clock = pygame.time.Clock()
 
-# Rectangles
-rect_1 = pygame.Rect(100, 300, 10, 100)
-rect_2 = pygame.Rect(1160, 300, 10, 100)
-
 # Colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BLUE = (0, 120, 255)
 
+# Game variables
+PADDLE_SPEED = 10
+BALL_SPEED = 2
+SPEED_BOOST = 1.1
+
+class Paddle:
+    def __init__(self, x, y, width, height, color):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color = color
+        self.rect.clamp_ip(screen.get_rect())
+
+    def move(self, up, down):
+        if up:
+            self.rect.y -= 5
+        if down:
+            self.rect.y += 5
+
+class Ball:
+    def __init__(self, x, y, radius):
+        self.rect = pygame.Rect(x, y, radius, radius)
+        self.x = BALL_SPEED
+        self.y = BALL_SPEED
+
+    def move(self, x, y):
+        self.rect.x += x
+        self.rect.y += y
+
+    def move(self):
+        self.rect.x += self.x
+        self.rect.y += self.y
+
+# Rectangles
+rect_1 = Paddle(100, 300, 10, 100, WHITE)
+rect_2 = Paddle(1160, 300, 10, 100, WHITE)
+
 # Ball
-ball = pygame.Rect(640, 360, 20, 20)
+ball = Ball(640, 360, 20)
 
 while running:
     for event in pygame.event.get():
@@ -32,18 +63,24 @@ while running:
     pygame.draw.rect(screen, WHITE, rect_2)
 
     # 3) draw ball
-    pygame.draw.rect(screen, BLUE, ball)
+    pygame.draw.rect(screen, BLUE, ball.rect)
 
     # 4) Move paddles
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        rect_1.y -= 5
-    if keys[pygame.K_s]:
-        rect_1.y += 5
-    if keys[pygame.K_UP]:
-        rect_2.y -= 5
-    if keys[pygame.K_DOWN]:
-        rect_2.y += 5
+    rect_1.move(keys[pygame.K_w], keys[pygame.K_s])
+    rect_2.move(keys[pygame.K_UP], keys[pygame.K_DOWN])
+
+    # 5) Move ball
+    ball.move()
+
+    # 6) Check for collisions
+    if ball.rect.colliderect(rect_1) or ball.rect.colliderect(rect_2):
+        ball.x *= -SPEED_BOOST
+        ball.y *= SPEED_BOOST
+
+    # 7) Check for out of bounds
+    if ball.rect.y < 0 or ball.rect.y > 720:
+        ball.y *= -1
 
     pygame.display.flip()
     clock.tick(60)
